e4ff9483b62bfb2968b76e1b3657d1ae6c914b93
Daniel Boydston <daniel.boydston@alyrica.net>
2018-03-26 14:53:03 -0700

    Log Times

--- stockr/be/src/run.php
+++ stockr/be/src/run.php
@@ -11,23 +11,23 @@
         if(is_numeric($interval)==false || $interval <= 0) {
             $interval=300;
         }
-        print("getting quotes...\n");
+        print(date("Y-m-d H:i:s")." getting quotes...\n");
         $result = StockR::getQuotes();
         if($result['result']!='success') {
             print_r($result);
         }
-        print("processing triggers...\n");
+        print(date("Y-m-d H:i:s")."processing triggers...\n");
         $result = StockR::processTriggers();
         if($result['result']!='success') {
             print_r($result);
         }
-        print("sending notifications...\n");
+        print(date("Y-m-d H:i:s")."sending notifications...\n");
         $result = StockR::sendNotifications();
         if($result['result']!='success') {
             print_r($result);
         }
 
-        print("sleeping $interval seconds...");
+        print(date("Y-m-d H:i:s")."sleeping $interval seconds...\n");
         sleep ($interval);
     }
 ?>
